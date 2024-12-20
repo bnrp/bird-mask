b986aad4586cc76da5f258c81f442310effcd571
Ben Rippley <rippley@gatech.edu>
2024-12-20 16:35:46 +0000

    Added hierarchical class labelling

--- bird-mask-keras.py
+++ bird-mask-keras.py
@@ -1,7 +1,7 @@
 import nabirds
 from nabirdsDataset import nabirdsDataset
 from utils import load_train_test, timer, load_data
-from nabirdsDataset_keras import load_images
+from nabirdsDataset_keras import load_images, load_images_with_hierarchy
 
 import time
 import os
@@ -14,9 +14,6 @@ import numpy as np
 import tensorflow as tf
 #import torch
 
-tf.autograph.set_verbosity(
-    level=0, alsologtostdout=False
-)
 
 def init_model(include_top=False, weights="imagenet", input_tensor=None, input_shape=None, pooling=None, classes=1000, activation="softmax", name="resnet50v2"):
     model = ResNet50V2(
@@ -46,7 +43,7 @@ def main():
     seed = 0
     split = 0.8
     lr = 1e-3
-    epochs = 3
+    epochs = 100
 
 
     # Dataset locations
@@ -55,7 +52,7 @@ def main():
 
 
     # Load data
-    training_set, validation_set = load_images(
+    training_set, validation_set = load_images_with_hierarchy(
                 dir=image_path,
                 batch_size=batch_size,
                 img_size=img_size,
@@ -63,33 +60,45 @@ def main():
                 split=split,
             )
 
+    #print(training_set)
+
     num_classes = training_set.element_spec[1].shape[1]
 
 
     # Create model
     res_model = init_model(
+                weights=None,
                 input_shape=input_size,
                 classes=num_classes,
             )
 
-    for layer in res_model.layers[:143]:
-        layer.trainable = False
+    #for layer in res_model.layers[:143]:
+    #    layer.trainable = False
 
 
     model = keras.models.Sequential()
     model.add(res_model)
     model.add(keras.layers.Flatten())
     model.add(keras.layers.BatchNormalization())
-    model.add(keras.layers.Dense(num_classes, activation='relu'))
+    model.add(keras.layers.Dense(num_classes*8, activation='relu'))
+    model.add(keras.layers.Dropout(0.5))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Dense(num_classes*4, activation='relu'))
+    model.add(keras.layers.Dropout(0.5))
+    model.add(keras.layers.BatchNormalization())
+    model.add(keras.layers.Dense(num_classes*2, activation='relu'))
     model.add(keras.layers.Dropout(0.5))
     model.add(keras.layers.BatchNormalization())
     model.add(keras.layers.Dense(num_classes, activation='softmax'))
 
    
+    tf.autograph.set_verbosity(
+        level=0, alsologtostdout=False
+    )
 
     # Train model
     model.compile(loss='categorical_crossentropy',
-                  optimizer=keras.optimizers.Adam(learning_rate=0.005),
+                  optimizer=keras.optimizers.Adam(learning_rate=lr),
                   metrics=['accuracy'])
     print(model.summary())
     history = model.fit(training_set, epochs=epochs, verbose=1, validation_data=validation_set)
--- nabirdsDataset_keras.py
+++ nabirdsDataset_keras.py
@@ -1,15 +1,22 @@
 import os
 import sys
 import keras
+import pydot
+import numpy as np
+import networkx as nx
+from networkx.drawing.nx_pydot import graphviz_layout
+import matplotlib.pyplot as plt
+
 
 dataset_path = 'nabirds-data/nabirds/'
 image_path = dataset_path + 'images/'
 
 
-def load_images(dir=image_path, batch_size=32, img_size=(256, 256), seed=0, split=0.8):
+
+def load_images(dir=image_path, labels="inferred", batch_size=32, img_size=(256, 256), seed=0, split=0.8):
     training, validation = keras.utils.image_dataset_from_directory(
                 directory = dir,
-                labels = "inferred",
+                labels = labels,
                 label_mode = "categorical",
                 class_names = None,
                 color_mode = "rgb",
@@ -30,5 +37,59 @@ def load_images(dir=image_path, batch_size=32, img_size=(256, 256), seed=0, spli
     return training, validation
 
 
+
+def create_hierarchical_labels(file='nabirds-data/nabirds/hierarchy.txt', draw=False):
+    G = nx.read_edgelist(file, create_using=nx.DiGraph(), nodetype=int)
+    G = G.reverse()
+
+    if draw:
+        pos = graphviz_layout(G, prog="dot")
+        nx.draw(G, pos)
+        plt.show()
+
+    topological_order = list(nx.topological_sort(G))
+    root = topological_order[0]
+
+    hierarchical_classes = list(G.successors(root))
+    leaf_classes = [n for n in G.nodes if G.out_degree(n)==0]
+
+    hierarchy_descendents = {n: nx.descendants(G, n) for n in hierarchical_classes}
+
+    for n in hierarchical_classes:
+        hierarchy_descendents[n] = set(leaf_classes) & set(hierarchy_descendents[n])
+
+    correct_dir_labels = np.array(leaf_classes)
+
+    for n in hierarchical_classes:
+        for m in hierarchy_descendents[n]:
+            idx = np.where(correct_dir_labels == m)
+            correct_dir_labels[idx] = n
+
+    correct_labels = []
+
+    for i in range(len(leaf_classes)):
+        leaf = leaf_classes[i]
+        folder = str(leaf).zfill(4)
+        count = len(os.listdir(image_path+folder+'/'))
+
+        correct_labels.extend([correct_dir_labels[i]]*count)
+
+    print(os.walk(image_path))
+
+    np.savetxt('labels/general_class_labels.txt', correct_labels)
+
+
+
+def load_images_with_hierarchy(label_dir='labels/general_class_labels.txt', dir=image_path, labels="inferred", batch_size=32, img_size=(256,256), seed=0, split=0.8):
+    correct_labels = list(np.loadtxt(label_dir).astype(int))
+
+    training, validate = load_images(dir, correct_labels, batch_size, img_size, seed, split)
+
+    return training, validate
+
+    
+
+
 if __name__ == "__main__":
-    load_images()
+    create_hierarchical_labels()
+    load_images_with_hierarchy()
